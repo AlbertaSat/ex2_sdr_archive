cf90c964dfc6041e52b623c20e5428cb60213ec1
dinethjay <jayaseke@ualberta.ca>
2020-12-04 12:00:42 -0700

    Got the CRC16 block to work! Yay!

--- gr-albertasat/lib/CRC16_Block_impl.cc
+++ gr-albertasat/lib/CRC16_Block_impl.cc
@@ -29,7 +29,7 @@
  #include <iostream>   // for std::cout
  #include <ostream>    // for std::endl
 #include "CRC16_Block_impl.h"
-
+bool d_check = 0;
 namespace gr {
   namespace albertasat {
 
@@ -47,7 +47,9 @@ namespace gr {
       : gr::tagged_stream_block("CRC16_Block",
               gr::io_signature::make(1, 1, sizeof(char)),
               gr::io_signature::make(1, 1, sizeof(char)), len_tag_key)
-    {}
+    {
+      set_tag_propagation_policy(TPP_DONT);
+    }
 
     /*
      * Our virtual destructor.
@@ -76,16 +78,31 @@ namespace gr {
 
       // specify result here
       boost::crc_ccitt_type result;
-      result.process_bytes(in, 6); // process the specified number of bytes in the input
+      result.process_bytes(in, packet_length); // process the specified number of bytes in the input
       crc = result.checksum();
       std::cout << "CRC16 Result is = " << std::hex << crc << std::endl; // output result in hex form
       printf("\n");
+
+      //printf("little endian CRC is: %X", crc);
+      crc = (crc>>8) | (crc<<8); // converting little endian to big endian
+      //printf("big endian CRC is: %X", crc);
       memcpy((void*)out, (const void*)in, packet_length);
       memcpy((void*)(out + packet_length),&crc, 2);
-      // Do <+signal processing+>
 
+      // Do <+signal processing+>
+      std::vector<tag_t> tags;
+      get_tags_in_range(tags, 0, nitems_read(0), nitems_read(0) + packet_length);
+      for (size_t i = 0; i < tags.size(); i++) {
+      tags[i].offset -= nitems_read(0);
+      if (d_check &&
+              tags[i].offset > (unsigned int)(packet_length + 2)) {
+      tags[i].offset = packet_length - 2 - 1;
+      }
+      add_item_tag(0, nitems_written(0) + tags[i].offset, tags[i].key, tags[i].value);
+      }
       // Tell runtime system how many output items we produced.
-      return noutput_items;
+      //return noutput_items;
+      return packet_length + 2;
     }
 
   } /* namespace albertasat */
